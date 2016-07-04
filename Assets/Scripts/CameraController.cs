9803ca84f1afaf8693e47b2dc92df0d6ced34a4c
nGenius <me@ngenius.kr>
2016-07-04 23:58:55 +0900

    [ Project Copy ] Roll a Ball

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offest;
+
+	// Use this for initialization
+	void Start () {
+        offest = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = player.transform.position + offest;
+	}
+}
